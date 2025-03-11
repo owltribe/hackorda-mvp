28491155eb9926e3f40c149e851a3c36459bef92
Aida <aidas2211@gmail.com>
2025-03-11 18:35:40 +0500

    Update Tanstack Query

--- src/app/_tanstack-providers.tsx
+++ src/app/_tanstack-providers.tsx
@@ -3,9 +3,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState, ReactNode } from 'react';
 
-export function QueryProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
 
+  const queryClient = new QueryClient();
+  
+  export function QueryProvider({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
