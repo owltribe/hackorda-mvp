e2c9aa460727b70de9a9bbf031d74a27f90c03c9
Aida <aidas2211@gmail.com>
2025-03-11 23:53:29 +0500

    updated

--- src/app/_tanstack-providers.tsx
+++ src/app/_tanstack-providers.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState, ReactNode } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactNode } from 'react';
 
-
-  const queryClient = new QueryClient();
+ const queryClient = new QueryClient();
   
   export function QueryProvider({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    
     </QueryClientProvider>
   );
 }
