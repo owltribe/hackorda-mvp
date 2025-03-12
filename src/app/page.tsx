51697b5f718bb56bd50f2853dfcaa60233ca2589
Aida <aidas2211@gmail.com>
2025-03-12 16:02:51 +0500

    Set Up Tanstack Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+'use client'
+
 import Image from "next/image";
+import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
+  function StudentList() {
+    const { data, isLoading } = useQuery({
+      queryKey: ['students'],
+      queryFn: () => fetch('https://jsonplaceholder.typicode.com/users/1').then(res => res.json()), 
+    });
+
+    if (isLoading) return <div>Загрузка...</div>;
+    return <div>Студент: {data?.name}</div>;
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -22,7 +34,8 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <StudentList />
+      
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -94,8 +107,10 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
+        
         </a>
       </footer>
     </div>
+  
   );
 }
