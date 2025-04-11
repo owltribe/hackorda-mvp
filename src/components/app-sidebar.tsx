611695ed8c2ad79ddff953031b7f698e29511773
Anuar Kopzhassarov <chehve@gmail.com>
2025-04-11 23:36:38 +0500

    fix bugs; add sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import * as React from "react"
+import {
+  Home,
+  User,
+  Command,
+  Send,
+  LifeBuoy,
+  Trophy
+} from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+} from "@/components/ui/sidebar"
+import { Button } from "./ui/button";
+import { SidebarAuth } from "@/components/sidebar-auth"
+import Link from "next/link";
+
+const navItems = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+    isActive: true
+  },
+  {
+    title: "Profile",
+    url: "/profile",
+    icon: User,
+  },
+  {
+    title: "Leaderboard",
+    url: "/leaderboard",
+    icon: Trophy,
+  },
+];
+
+const secondaryItems = [
+  {
+    title: "Support",
+    url: "#",
+    icon: LifeBuoy,
+  },
+  {
+    title: "Feedback",
+    url: "#",
+    icon: Send,
+  }
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar 
+      variant="floating" 
+    >
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-purple-700 text-sidebar-primary-foreground">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">HackOrda MVP</span>
+                  <span className="truncate text-xs">Quiz App</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              
+              <Link href="/quiz">
+                <Button variant="custom" className="w-full cursor-pointer dark:text-white">Take New Quiz</Button>
+              </Link>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild className="hover:bg-purple-100 py-5 border-b border-purple-200 dark:hover:bg-purple-900">
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        
+        <SidebarGroup className="mt-auto">
+          <SidebarGroupLabel>More</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {secondaryItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild className="hover:bg-purple-100 py-5 border-b border-purple-200 dark:hover:bg-purple-900">
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarAuth />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
